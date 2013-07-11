41b6bdc2fd856d45c6b8bf46c8003b9fd3bd80d3
qibo <qibo@sb>
2013-07-11 15:27:13 +0800

    zhangqibo: update code

--- showkeyHelper/gen/android/content/pm/IPackageStatsObserver.java
+++ showkeyHelper/gen/android/content/pm/IPackageStatsObserver.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: D:\\AndroidWorkSpace\\showkeyHelper\\src\\android\\content\\pm\\IPackageStatsObserver.aidl
+ * Original file: E:\\git\\show-client-helper\\showkeyHelper\\src\\android\\content\\pm\\IPackageStatsObserver.aidl
  */
 package android.content.pm;
 /***
