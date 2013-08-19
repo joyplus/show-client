47e52069bbcb2ed2427e053de75d21bb835cb791
qibo <qibo@sb>
2013-08-19 19:25:09 +0800

    zhangqibo:update code

--- showkeyHelper/src/com/joyplus/tvhelper/BootCompleteReceiver.java
+++ showkeyHelper/src/com/joyplus/tvhelper/BootCompleteReceiver.java
@@ -3,6 +3,7 @@ package com.joyplus.tvhelper;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 import com.joyplus.tvhelper.faye.FayeService;
 import com.joyplus.tvhelper.utils.PreferencesUtils;
@@ -12,7 +13,7 @@ public class BootCompleteReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		// TODO Auto-generated method stub
-		if(PreferencesUtils.getPincodeMd5(context)!=null){
+		if(PreferencesUtils.getPincode(context)!=null){
 			context.startService(new Intent(context, FayeService.class));
 		}
 	}
