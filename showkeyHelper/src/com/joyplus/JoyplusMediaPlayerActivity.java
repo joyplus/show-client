c61b20a94b6aff039e741228a46314745a639977
jaselisan <jas.sheng@joyplus.tv>
2013-11-24 20:51:14 +0800

    Add Video Player auto restore feature.

--- showkeyHelper/src/com/joyplus/JoyplusMediaPlayerActivity.java
+++ showkeyHelper/src/com/joyplus/JoyplusMediaPlayerActivity.java
@@ -146,6 +146,8 @@ public class JoyplusMediaPlayerActivity extends Activity implements JoyplusMedia
 						m.arg1 = (int) seekto;
 						PlayerHandler.sendMessage(m);
 					}else{
+						if(mVideoView.getPlayer() == null)
+							mVideoView.update();
 						if(mVideoView.getPlayer() != null){
 							mVideoView.getPlayer().SetVideoPaths(path);
 							if(lastTime>0){
