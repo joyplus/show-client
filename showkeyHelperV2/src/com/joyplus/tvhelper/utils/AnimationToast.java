f0aa91cb9726f3123ccacdb6843c6b9303f87012
xiaoyunqiang <monkey.xiao@joyplus.tv>
2013-12-09 13:47:52 +0800

    level and toast

---
+++ showkeyHelperV2/src/com/joyplus/tvhelper/utils/AnimationToast.java
@@ -0,0 +1,124 @@
+package com.joyplus.tvhelper.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import com.joyplus.tvhelper.R;
+
+public class AnimationToast {
+	static final String TAG = "AnimationToast";    
+    public static final int LENGTH_SHORT = 0;  // 显示时间长短
+    public static final int LENGTH_LONG = 1;  
+    
+    final Context mContext;  
+    int mDuration;  
+    PopupWindow mPopToast;  
+    View mParent;  
+    int width = 300;//toast 宽  
+    int height =60;//toast 高  
+      
+    public AnimationToast(Context context)  
+    {  
+        mContext = context;  
+    }  
+      
+    public void show()  
+    {  
+        mPopToast.showAtLocation(mParent, Gravity.TOP|Gravity.RIGHT,0, 35);  
+          
+        //LONG��5000ms    SHORT��300ms  
+        //显示时间等于动画时间加上不变时间
+        long duration = mDuration== LENGTH_LONG ? 5000 : 1000;  
+          
+        mParent.postDelayed(new Runnable()  
+        {  
+            @Override  
+            public void run()  
+            {  
+                cancel();  
+            }  
+        }, duration);  
+    }  
+      
+    public void cancel()  
+    {  
+        mPopToast.dismiss();  
+    }  
+      
+    /** 
+     * 设置toast 宽高 动画 背景 
+     */  
+    public void setView(View view)   
+    {  
+    	
+        mPopToast = new PopupWindow(view, width, height);  
+        mPopToast.setAnimationStyle(R.style.AnimationToast);  
+        mPopToast.setBackgroundDrawable(mContext.getResources().getDrawable(R.drawable.default_bg));  
+        mPopToast.setFocusable(false);  
+        mPopToast.setOutsideTouchable(true);  
+    }  
+  
+    /** 
+     *获取视图
+     */  
+    public View getView()   
+    {  
+        return mPopToast.getContentView();  
+    }  
+      
+    public void setParent(View parent)  
+    {  
+        mParent = parent;  
+    }  
+      
+    public View getParent()  
+    {  
+        return mParent;  
+    } 
+    public void setDuration(int duration)   
+    {  
+        mDuration = duration;  
+    }  
+    public int getDuration()   
+    {  
+        return mDuration;  
+    }  
+      
+    public void setWidth(int w)  
+    {  
+        width = w;  
+    }  
+      
+    public void setHeight(int h)  
+    {  
+        height =h;  
+    } 
+    public static AnimationToast makeText(Context context, CharSequence text, int duration, View parent)   
+    {  
+    	Log.d(TAG,"makeText"+text+"duration"+duration+"parent"+parent);
+        AnimationToast result = new AnimationToast(context);  
+  
+        TextView tv = new TextView(context);
+        tv.setTextSize(25);
+        tv.setTextColor(Color.WHITE);  
+        tv.setGravity(Gravity.CENTER);  
+        tv.setText(text);  
+          
+        result.setView(tv);  
+        result.setParent(parent);  
+        result.setDuration(duration);  
+        
+        return result;  
+    }  
+    public static AnimationToast makeText(Context context, int resId, int duration, View parent)  
+                                throws Resources.NotFoundException {  
+        return makeText(context, context.getResources().getText(resId), duration, parent);  
+    }  
+
+}
---
+++ showkeyHelperV2/src/com/joyplus/tvhelper/utils/LevelMore.java
@@ -0,0 +1,140 @@
+package com.joyplus.tvhelper.utils;
+
+import com.joyplus.tvhelper.R;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.RelativeLayout.LayoutParams;
+
+public class LevelMore extends RelativeLayout{
+
+	private static final String TAG = "test";
+	private Context mContext;
+	private TextView oneTextView,twoTextView,homeTextView,fristPoint,secondPoint;
+	private String get_level_string,set_level_string;
+	
+	
+	
+	public LevelMore(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+		mContext  = context;
+		InitResource();
+	}
+
+	public LevelMore(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+		mContext  = context;
+		InitResource(); 
+	}
+
+	public LevelMore(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+		mContext  = context;
+		
+		InitResource();
+		
+	}
+
+	private void InitResource() {
+		// TODO Auto-generated method stub
+		//LayoutInflater inflater=(LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LayoutInflater inflater = LayoutInflater.from(mContext);
+		//LayoutInflater.from(c).inflate(R.layout.item_layout_gallery, null);
+        View v = inflater.inflate(R.layout.levelmore_layout, null);
+        this.addView(v,new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT));
+        oneTextView = (TextView) v.findViewById(R.id.level_one);
+        twoTextView = (TextView) v.findViewById(R.id.level_two);
+        homeTextView = (TextView) v.findViewById(R.id.level_home);
+        fristPoint = (TextView) v.findViewById(R.id.frist_level_point);
+        secondPoint = (TextView) v.findViewById(R.id.second_level_point);
+        Log.d(TAG,"INIT");
+   }
+
+	public void getFristLevel(String getlevel){
+		get_level_string=getlevel;
+		AnimationSet animationSet = new AnimationSet(true);
+		AlphaAnimation alphaAnimation = new AlphaAnimation(0,1);
+		alphaAnimation.setDuration(250);
+		animationSet.addAnimation(alphaAnimation);
+		Animation moreAnimationRight = new TranslateAnimation(Animation.RELATIVE_TO_SELF,-2f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f);
+		moreAnimationRight.setDuration(300);
+		animationSet.addAnimation(moreAnimationRight);
+		homeTextView.startAnimation(animationSet);
+		homeTextView.setVisibility(View.VISIBLE);
+		fristPoint.startAnimation(animationSet);
+		fristPoint.setVisibility(View.VISIBLE);
+		oneTextView.startAnimation(animationSet);
+		oneTextView.setVisibility(View.VISIBLE);
+		oneTextView.setText(get_level_string);
+		Log.d(TAG,"getFristLevel------->");
+		
+	}
+	
+
+	public void getSecondLevel(String getlevel){
+		get_level_string=getlevel;
+		AnimationSet animationSet = new AnimationSet(true);
+		AlphaAnimation alphaAnimation = new AlphaAnimation(0,1);
+		alphaAnimation.setDuration(250);
+		animationSet.addAnimation(alphaAnimation);
+		Animation moreAnimationRight = new TranslateAnimation(Animation.RELATIVE_TO_SELF,-2f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f);
+		moreAnimationRight.setDuration(500);
+		animationSet.addAnimation(moreAnimationRight);
+		twoTextView.startAnimation(animationSet);
+		secondPoint.startAnimation(animationSet);
+		secondPoint.setVisibility(View.VISIBLE);
+		twoTextView.setVisibility(View.VISIBLE);
+		twoTextView.setText(get_level_string);
+		Log.d(TAG,"getSecondLevel------->");
+	}
+    public void dismissFristLevel(){
+    	AnimationSet lanimationSet = new AnimationSet(true);
+		AlphaAnimation lalphaAnimation = new AlphaAnimation(1,0);
+		lalphaAnimation.setDuration(500);
+		lanimationSet.addAnimation(lalphaAnimation);
+		Animation lessAnimationRight = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,-10f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f);
+		lessAnimationRight.setDuration(1500);
+		lanimationSet.addAnimation(lessAnimationRight);
+		lanimationSet.setFillAfter(true);
+		homeTextView.startAnimation(lanimationSet);
+		homeTextView.setVisibility(View.VISIBLE);
+		fristPoint.startAnimation(lanimationSet);
+		fristPoint.setVisibility(View.VISIBLE);
+		oneTextView.startAnimation(lanimationSet);
+		oneTextView.setVisibility(View.VISIBLE);
+		Log.d(TAG,"dismissFristLevel------->");
+	}
+	public void dismissSecondLevel(){
+		AnimationSet lanimationSet = new AnimationSet(true);
+		AlphaAnimation lalphaAnimation = new AlphaAnimation(1,0);
+		lalphaAnimation.setDuration(500);
+		lanimationSet.addAnimation(lalphaAnimation);
+		Animation lessAnimationRight = new TranslateAnimation(Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,-10f,Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f);
+		lessAnimationRight.setDuration(1500);
+		lanimationSet.addAnimation(lessAnimationRight);
+		lanimationSet.setFillAfter(true);
+		secondPoint.startAnimation(lanimationSet);
+		secondPoint.setVisibility(View.VISIBLE);
+		twoTextView.startAnimation(lanimationSet);
+		twoTextView.setVisibility(View.VISIBLE);
+		Log.d(TAG,"dismissSecondLevel------->");
+	}
+}
+
+
